5cdca788162c6803e2eba059bf6acde1b815f505
GeorgeBronner <78767238+GeorgeBronner@users.noreply.github.com>
2025-04-09 21:06:15 -0500

    fix env for BACKEND_SERVER_IP

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 console.log('VITE_SERVER_IP value:', process.env.VITE_SERVER_IP);
 
-const SERVER_IP = process.env.VITE_SERVER_IP || '127.0.0.1';
+const BACKEND_SERVER_IP = process.env.BACKEND_SERVER_IP || '127.0.0.1';
 
-export const API_BASE_URL = `http://${SERVER_IP}:8005`; 
+export const API_BASE_URL = `http://${BACKEND_SERVER_IP}:8005`;
